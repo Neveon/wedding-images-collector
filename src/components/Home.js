5cf4683e15a48428e0e2ad8971f409005680e8f8
Neil T <neiltz711@gmail.com>
2019-07-10 18:13:29 -0400

    Deploy test

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+function Home() {
+  const handleFile = e => {
+    const totalFiles = e.target.files.length;
+    for (let x = 0; x < totalFiles; x++) {
+      let image = e.target.files[x];
+      let formData = new FormData();
+      formData.append('image', image, image.name);
+      axios.post('/image', formData).catch(err => console.log(err));
+    }
+  };
+
+  return (
+    <div>
+      <input type='file' onChange={handleFile} />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/UploadedImages.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function UploadedImages() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/images')
+      .then(res => {
+        setList(res.data.imageUrl);
+      })
+      .catch(err => console.log(err));
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(list);
+
+  return list.map((imageUrl, index) => {
+    return <img src={imageUrl} key={index} />;
+  });
+}
+
+export default UploadedImages;

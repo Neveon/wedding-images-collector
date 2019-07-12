610cd8cf74e60efd0f3db653a2e7dfc0fc706d6c
Neil T <neiltz711@gmail.com>
2019-07-11 21:03:38 -0400

    Prepare Deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-function Home() {
-  const handleFile = e => {
+// Home pic
+import weddingPhoto from '../images/weddingHomePhoto.jpg';
+
+// Material-UI
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
+class Home extends Component {
+  handleFile = e => {
     const totalFiles = e.target.files.length;
     for (let x = 0; x < totalFiles; x++) {
       let image = e.target.files[x];
@@ -18,14 +25,48 @@ function Home() {
     }
   };
 
-  return (
-    <div>
-      <input type='file' onChange={handleFile} />
-      <br />
-      <br />
-      <Link to='/images'>See images</Link>
-    </div>
-  );
+  handleEdit = () => {
+    const fileInput = document.getElementById('imageInput');
+    // Opens file selection and when user selects a file, triggers handleImageChange
+    fileInput.click();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='homeContainer'>
+          <h1 className='title'>
+            Nageatte and Alaa's Wedding Reception
+            <br />
+            July 12, 2019
+          </h1>
+          <img
+            src={weddingPhoto}
+            style={{ width: '100%' }}
+            alt='weddingPhotoHome'
+            id='weddingPhotoHome'
+          />
+          <input
+            type='file'
+            id='imageInput'
+            onChange={this.handleFile}
+            hidden='hidden'
+          />
+          <Button variant='contained' color='primary' onClick={this.handleEdit}>
+            Upload Images&nbsp;
+            <CloudUploadIcon />
+          </Button>
+          <br />
+          <br />
+          <Link to='/images'>
+            <Button variant='contained' color='secondary'>
+              See images
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/UploadedImages.js
+++ src/components/UploadedImages.js
@@ -1,26 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 function UploadedImages() {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [textInput, setTextInput] = useState('');
+  const [err, setErr] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://us-central1-image-upload-9aebe.cloudfunctions.net/api/images'
-      )
-      .then(res => {
-        setList(res.data.imageUrl);
-      })
-      .catch(err => console.log(err));
-    // eslint-disable-next-line
-  }, []);
+  const onChange = e => {
+    setTextInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setErr(false);
+
+    if (textInput === 'pass') {
+      setLoading(true);
+      axios
+        .get(
+          'https://us-central1-image-upload-9aebe.cloudfunctions.net/api/images'
+        )
+        .then(res => {
+          setList(res.data.imageUrl);
+        })
+        .catch(err => console.log(err));
+      setLoading(false);
+      //console.log(loading);
+    } else {
+      setErr(true);
+    }
+  };
 
   console.log(list);
 
-  return list.map((imageUrl, index) => {
-    return <img src={imageUrl} key={index} alt='weddingPhoto' />;
-  });
+  return list.length ? (
+    !loading ? (
+      list.map((imageUrl, index) => {
+        return <img src={imageUrl} key={index} alt={`weddingPhoto ${index}`} />;
+      })
+    ) : (
+      <p>Loading...</p>
+    )
+  ) : (
+    <div>
+      <p>Please Enter Passcode to View Images</p>
+      <form onSubmit={handleSubmit}>
+        <Input type='text' onChange={onChange} value={textInput} />
+        <br />
+        <br />
+        <Button type='submit' variant='contained' color='primary'>
+          Submit
+        </Button>
+      </form>
+      <br />
+      {err ? <p style={{ color: 'red' }}>Wrong Password</p> : null}
+    </div>
+  );
 }
 
 export default UploadedImages;
